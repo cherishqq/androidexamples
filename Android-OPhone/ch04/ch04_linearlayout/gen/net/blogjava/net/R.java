5a59d84504cd6ac05551815a86144ebf6803d0ea
derek <haijinme@qq.com>
2012-11-30 14:37:35 +0800

    do some test on Linearlayout

--- Android-OPhone/ch04/ch04_linearlayout/gen/net/blogjava/net/R.java
+++ Android-OPhone/ch04/ch04_linearlayout/gen/net/blogjava/net/R.java
@@ -12,9 +12,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int main=0x7f020001;
+        public static final int main_compose=0x7f020002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int main_add=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- Android-OPhone/ch04/ch04_linearlayout/src/net/blogjava/net/Main.java
+++ Android-OPhone/ch04/ch04_linearlayout/src/net/blogjava/net/Main.java
@@ -3,12 +3,18 @@ package net.blogjava.net;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+/**
+ * Study: 2012-11-30
+ * @author Derek.pan
+ *
+ */
 public class Main extends Activity
 {
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);		
+		setContentView(R.layout.main_add);		
 	}
 }
