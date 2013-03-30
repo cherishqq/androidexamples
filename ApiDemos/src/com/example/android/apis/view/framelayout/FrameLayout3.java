d9541b5e3e4cb1a0532541c8900a8481bf64886a
derek <haijinme@qq.com>
2013-03-30 11:46:03 +0800

    add merge tags support

---
+++ ApiDemos/src/com/example/android/apis/view/framelayout/FrameLayout3.java
@@ -0,0 +1,17 @@
+package com.example.android.apis.view.framelayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.android.apis.R;
+
+public class FrameLayout3 extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.framelayout_3);
+	}
+
+}
